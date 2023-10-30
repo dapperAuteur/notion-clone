590a1ae3b66518b283896dd43e3f91f96d15faa5
awe <aweful@awews.com>
2023-10-30 16:38:53 -0700

    allow logged in user to create document

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Toaster } from "sonner";
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
@@ -41,7 +42,8 @@ export default function RootLayout({
             disableTransitionOnChange
             storageKey='jotion-theme'
             >
-            {children}
+              <Toaster position="bottom-center" />
+              {children}
           </ThemeProvider>
         </ConvexClientProvider>
       </body>
