bb920c8effed30a7178120cbdd33fe3bd0e8ac01
awe <aweful@awews.com>
2023-10-30 20:05:17 -0700

    add the ability to archive documents and all its children

--- convex/documents.ts
+++ convex/documents.ts
@@ -3,55 +3,55 @@ import { v } from "convex/values";
 import { mutation, query } from "./_generated/server";
 import { Doc, Id } from "./_generated/dataModel";
 
-// export const archive = mutation({
-//   args: { id: v.id("documents") },
-//   handler: async (ctx, args) => {
-//     const identity = await ctx.auth.getUserIdentity();
+export const archive = mutation({
+  args: { id: v.id("documents") },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
 
-//     if (!identity) {
-//       throw new Error("Not authenticated");
-//     }
+    if (!identity) {
+      throw new Error("Not authenticated");
+    }
 
-//     const userId = identity.subject;
+    const userId = identity.subject;
 
-//     const existingDocument = await ctx.db.get(args.id);
+    const existingDocument = await ctx.db.get(args.id);
 
-//     if (!existingDocument) {
-//       throw new Error("Not found");
-//     }
+    if (!existingDocument) {
+      throw new Error("Not found");
+    }
 
-//     if (existingDocument.userId !== userId) {
-//       throw new Error("Unauthorized");
-//     }
+    if (existingDocument.userId !== userId) {
+      throw new Error("Unauthorized");
+    }
 
-//     const recursiveArchive = async (documentId: Id<"documents">) => {
-//       const children = await ctx.db
-//         .query("documents")
-//         .withIndex("by_user_parent", (q) => (
-//           q
-//             .eq("userId", userId)
-//             .eq("parentDocument", documentId)
-//         ))
-//         .collect();
+    const recursiveArchive = async (documentId: Id<"documents">) => {
+      const children = await ctx.db
+        .query("documents")
+        .withIndex("by_user_parent", (q) => (
+          q
+            .eq("userId", userId)
+            .eq("parentDocument", documentId)
+        ))
+        .collect();
 
-//       for (const child of children) {
-//         await ctx.db.patch(child._id, {
-//           isArchived: true,
-//         });
+      for (const child of children) {
+        await ctx.db.patch(child._id, {
+          isArchived: true,
+        });
 
-//         await recursiveArchive(child._id);
-//       }
-//     }
+        await recursiveArchive(child._id);
+      }
+    }
 
-//     const document = await ctx.db.patch(args.id, {
-//       isArchived: true,
-//     });
+    const document = await ctx.db.patch(args.id, {
+      isArchived: true,
+    });
 
-//     recursiveArchive(args.id);
+    recursiveArchive(args.id);
 
-//     return document;
-//   }
-// })
+    // return document;
+  }
+})
 
 export const getSidebar = query({
   args: {
