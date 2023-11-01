b41177b4b3419c009c819ac0ac7593d6c82aa9bf
awe <aweful@awews.com>
2023-10-31 17:43:42 -0700

    add banner and menu to navbar to allow deleting of documents inside doc

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -8,9 +8,9 @@ import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 
 import { Title } from "./title";
-// import { Banner } from "./banner";
-// import { Menu } from "./menu";
-// import { Publish } from "./publish";
+import { Banner } from "./banner";
+import { Menu } from "./menu";
+import { Publish } from "./publish";
 
 interface NavbarProps {
   isCollapsed: boolean;
@@ -32,7 +32,7 @@ export const Navbar = ({
       <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center justify-between">
         <Title.Skeleton />
         <div className="flex items-center gap-x-2">
-          {/* <Menu.Skeleton /> */}
+          <Menu.Skeleton />
         </div>
       </nav>
     )
@@ -55,14 +55,14 @@ export const Navbar = ({
         <div className="flex items-center justify-between w-full">
           <Title initialData={document} />
           <div className="flex items-center gap-x-2">
-            {/* <Publish initialData={document} />
-            <Menu documentId={document._id} /> */}
+            <Publish initialData={document} />
+            <Menu documentId={document._id} />
           </div>
         </div>
       </nav>
-      {/* {document.isArchived && (
+      {document.isArchived && (
         <Banner documentId={document._id} />
-      )} */}
+      )}
     </>
   )
 }
