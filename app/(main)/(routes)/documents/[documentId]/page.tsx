4f5f48cafc02e620c955a866c6ca11990147c2f9
awe <aweful@awews.com>
2023-10-31 23:14:13 -0700

    remove DocumentId from document page

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -59,7 +59,6 @@ const DocumentIdPage = ({
     <div className="pb-40">
       <Cover url={document.coverImage} />
       <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
-        DocumentId
         <Toolbar initialData={document} />
         <Editor
           onChange={onChange}
