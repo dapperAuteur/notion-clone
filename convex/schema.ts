36cbb8c509a7276b9f77bc0d7b9519bc8097b0ac
awe <aweful@awews.com>
2023-11-04 18:18:13 -0700

    refactor schema to include slug for document and user model

--- convex/schema.ts
+++ convex/schema.ts
@@ -11,7 +11,19 @@ export default defineSchema({
     coverImage: v.optional(v.string()),
     icon: v.optional(v.string()),
     isPublished: v.boolean(),
+    slug: v.optional(v.string()),
   })
   .index("by_user", ["userId"])
-  .index("by_user_parent", ["userId", "parentDocument"])
+  .index("by_user_parent", ["userId", "parentDocument"]),
+  users: defineTable({
+    firstName: v.optional(v.string()),
+    lastName: v.optional(v.string()),
+    email: v.string(),
+    passwordHash: v.string(),
+    username: v.optional(v.string()),
+    image: v.optional(v.string()),
+    documentId: v.string(),
+  })
+  .index("by_email", ["email"])
+  .index("by_username", ["username"]),
 });
